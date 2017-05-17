8bdf37647abf439481f0b2ca7b088fa1330683d4
Chebrolu, Azeet <Azeet.Chebrolu@aa.com>
2017-05-17 13:22:15 -0700

    ejected ng cli

--- DEPLOY.bat
+++ DEPLOY.bat
@@ -1,10 +1,12 @@
-@rem Deploy to QA/DEV
+﻿@rem Deploy to QA/DEV
 @REM ECHO %1
 IF "%1" == "QA" (
-	robocopy dist \\MEAPPT53.QCORPAA.AA.COM\D$\HSDA\HSDA.UI /E *.*
+    ng build -e qa --output-hashing=all
+	robocopy dist \\MEAPPT53.QCORPAA.AA.COM\D$\HSDA\HSDA.UI /E *.* /PURGE
 	@REM ECHO "DEPLOY TO QA"
 	)
 IF "%1" == "DEV" (
-	robocopy dist \\MEAPND08.QCORPAA.AA.COM\D$\HSDA\HSDA.UI /E *.*
+    ng build --output-hashing=all
+	robocopy dist \\MEAPND08.QCORPAA.AA.COM\D$\HSDA\HSDA.UI /E *.* /PURGE
 	@REM ECHO "DEPLOY TO DEV"
 	)
