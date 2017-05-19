a45c2c0df1a34328632c761c138fcdb5f0692b12
Chebrolu, Azeet <Azeet.Chebrolu@aa.com>
2017-05-19 15:08:50 -0700

    corrected api endpoint

--- src/environments/environment.dev_env.ts
+++ src/environments/environment.dev_env.ts
@@ -1,6 +1,6 @@
 ﻿export const environment = {
   production: false,
   aircraftServiceEndPointUrl: 'http://10.62.99.171/AircraftService/api/aircraft/',
-  hsdaApiBaseUrl: 'http://hsda-services.test.techops.aa.com/',
+  hsdaApiBaseUrl: 'http://hsda-services.dev.techops.aa.com/',
   stationsEndPoint: 'api/stations'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,6 @@
 export const environment = {
   production: false,
   aircraftServiceEndPointUrl: 'http://10.62.99.190/AircraftService/api/aircraft/',
-  hsdaApiBaseUrl:'http://localhost/hsdaapi/',
+  hsdaApiBaseUrl:'http://hsda-services.dev.techops.aa.com/',
   stationsEndPoint: 'api/stations'
 };
