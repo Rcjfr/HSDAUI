67c1e2ab2eeed1ba5e057a9375eb7bae1345d194
Chebrolu, Azeet <Azeet.Chebrolu@aa.com>
2017-06-15 09:28:48 -0700

    updated reason for change dropdown

--- src/app/components/search/search-by-cpcp-disposition/search-by-cpcp-disposition.component.ts
+++ src/app/components/search/search-by-cpcp-disposition/search-by-cpcp-disposition.component.ts
@@ -10,7 +10,12 @@ import { ICorrosionLevel, IBaseLookUp } from '../../../common/models';
 })
 export class SearchByCpcpDispositionComponent implements OnInit {
   corrosionLevels$: Observable<List<ICorrosionLevel>>;
-  reasonsForChange$: Observable<IBaseLookUp[]>=Observable.of([]);
+  reasonsForChange$: Observable<IBaseLookUp[]> = Observable.of([
+    { id: 1, description: 'Incorrect per Corrosion Level Block Diagram' },
+    { id: 2, description: 'Evidence of a previous blend has been determined' },
+    { id: 3, description: 'New limits have been defined' },
+    { id: 4, description: 'Pre-blended measurements indicate Level - 1; elected to replace part for company convenience' }
+  ]);
   constructor(private appStateService: AppStateService) { }
 
   ngOnInit() {
