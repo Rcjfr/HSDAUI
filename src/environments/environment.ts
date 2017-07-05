ab6ab2cf0dbf8ab45b850aa6b20ddf7068385eea
poojitha jaya <poojitha.jaya@aa.com>
2017-07-05 10:22:15 -0700

    commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,5 +6,5 @@
 export const environment = {
   production: false,
   aircraftServiceEndPointUrl: 'http://10.62.99.190/AircraftService/api/aircraft/',
-  hsdaApiBaseUrl: 'http://hsda-services.dev.techops.aa.com/api/'
+  hsdaApiBaseUrl: 'http://localhost/hsdaapi/api/'
 };
