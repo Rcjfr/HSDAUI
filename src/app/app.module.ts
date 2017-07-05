e19961dde4afcfa25815f88f06994b1b8df81e48
poojitha jaya <poojitha.jaya@aa.com>
2017-07-05 11:21:28 -0700

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -76,7 +76,6 @@ import { DefectivePartDescriptionComponent } from './components/defective-part-d
 import { SearchByCpcpDispositionComponent } from './components/search/search-by-cpcp-disposition/search-by-cpcp-disposition.component';
 import { RepairDetailsSectionComponent } from './components/repair-details-section/repair-details-section.component';
 import { SearchByPartComponent } from './components/search/search-by-part/search-by-part.component';
-import { SearchByAircraftModelComponent } from './components/search/search-by-aircraft-model/search-by-aircraft-model.component';
 import { SearchOptionsComponent } from './components/search/search-options/search-options.component';
 
 @NgModule({
