99e4d4782efa3f45d1ecd10f95238931ae00f2fa
Chebrolu, Azeet <Azeet.Chebrolu@aa.com>
2017-07-24 11:57:23 -0700

    used bootstrap dialog for confirmation

--- src/app/common/components/pending-changes.guard.ts
+++ src/app/common/components/pending-changes.guard.ts
@@ -2,14 +2,16 @@
 import { Observable } from 'rxjs/Rx';
 import { ConfirmComponent } from './confirm/confirm.component';
 import { DialogService } from 'ng2-bootstrap-modal';
-import { ReflectiveInjector } from '@angular/core';
+import { Injectable } from "@angular/core";
 export interface ComponentCanDeactivate {
   canDeactivate: () => boolean | Observable<boolean>;
 }
-
+@Injectable()
 export class PendingChangesGuard implements CanDeactivate<ComponentCanDeactivate> {
 
-  //constructor(private dialogService: DialogService) {  }
+  constructor(private dialogService: DialogService) {
+    
+  }
   canDeactivate(component: ComponentCanDeactivate): boolean | Observable<boolean> {
     // if there are no pending changes, just allow deactivation; else confirm first
     return component.canDeactivate() ?
@@ -17,10 +19,10 @@ export class PendingChangesGuard implements CanDeactivate<ComponentCanDeactivate
       // NOTE: this warning message will only be shown when navigating elsewhere within your angular app;
       // when navigating away from your angular app, the browser will show a generic warning message
       // see http://stackoverflow.com/a/42207299/7307355
-      confirm('WARNING: You have unsaved changes. Press Cancel to go back and save these changes, or OK to lose these changes.');
-    //this.dialogService.addDialog(ConfirmComponent, {
-    //  title: 'Confirm?',
-    //  message: 'WARNING: You have unsaved changes. Press Cancel to go back and save these changes, or OK to lose these changes.'
-    //})
+    
+    this.dialogService.addDialog(ConfirmComponent, {
+      title: 'Confirm?',
+      message: 'WARNING: You have unsaved changes. Press Cancel to go back and save these changes, or OK to lose these changes.'
+    })
   }
 }
