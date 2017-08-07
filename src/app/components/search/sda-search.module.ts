67651d4ef14fa79ae1870003e469eccad704c538
Chebrolu, Azeet <Azeet.Chebrolu@aa.com>
2017-08-07 09:38:49 -0700

    lint errors

--- src/app/components/search/sda-search.module.ts
+++ src/app/components/search/sda-search.module.ts
@@ -30,7 +30,6 @@ import { SearchOptionsComponent } from './search-options/search-options.componen
     NgPipesModule,
     TypeaheadModule.forRoot(),
     FormControlsModule
-   
   ],
   exports: [
     SearchByDateRangeComponent,
