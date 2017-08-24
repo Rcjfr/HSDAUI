4c6c52bdd4c0163c2ac8d3bc9bbd59745638fed1
Malmgren, Melinda <melinda.malmgren@aa.com>
2017-08-24 10:40:57 -0700

    Added date formatters for date fields

--- src/app/components/search/search-by-date-range/search-by-date-range.component.ts
+++ src/app/components/search/search-by-date-range/search-by-date-range.component.ts
@@ -1,7 +1,8 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { SearchBaseFormComponent } from '../search-base-form.component';
 import { NKDatetime } from 'ng2-datetime/ng2-datetime';
+import * as moment from 'moment';
 
 @Component({
   selector: 'aa-search-by-date-range',
@@ -9,10 +10,19 @@ import { NKDatetime } from 'ng2-datetime/ng2-datetime';
   styleUrls: ['./search-by-date-range.component.less']
 })
 export class SearchByDateRangeComponent extends SearchBaseFormComponent {
+
   constructor(private formBuilder: FormBuilder) {
     super(formBuilder.group({
       'dateFrom': [undefined, Validators.required],
       'dateThrough': [undefined, Validators.required]
     }));
+  }
+
+  onDateFromChanged(event) {
+    this.model.dateFrom = moment(event).format('YYYY-MM-DD') + 'T00:00:00';
+  }
+
+  onDateThroughChanged(event) {
+    this.model.dateThrough = moment(event).format('YYYY-MM-DD') + 'T00:00:00';
   }
 }
