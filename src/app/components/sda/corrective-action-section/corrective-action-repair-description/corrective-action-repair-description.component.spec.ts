a6a810b9194d651dc46a6b260f904d22ffcac3a2
Malmgren, Melinda <melinda.malmgren@aa.com>
2017-09-14 09:58:48 -0700

    Enabling SDA form unit tests

---
+++ src/app/components/sda/corrective-action-section/corrective-action-repair-description/corrective-action-repair-description.component.spec.ts
@@ -0,0 +1,39 @@
+import { CorrectiveActionRepairDescriptionComponent } from './corrective-action-repair-description.component';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { AppStateService } from '../../../../common/services';
+import { MockAppStateService } from '../../../../common/services/mocks/mock-app-state.service';
+import { FormControlsModule } from '../../../../common/components/form-controls.module';
+import { ReactiveFormsModule, FormGroup } from '@angular/forms';
+import { SdaFormModule } from 'app/components/sda/sda-form.module';
+import * as models from '../../../../common/models';
+
+
+describe('SearchByCorrectiveActionComponent', () => {
+  let component: CorrectiveActionRepairDescriptionComponent;
+  let fixture: ComponentFixture<CorrectiveActionRepairDescriptionComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [],
+      imports: [ReactiveFormsModule, FormControlsModule, SdaFormModule ],
+      providers: [{ provide: AppStateService, useClass: MockAppStateService }]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CorrectiveActionRepairDescriptionComponent);
+    component = fixture.componentInstance;
+
+    //Inputs
+    component.parent = new FormGroup({});
+    component.sda = {};
+    component.newSdaStus = models.Status.Open;
+
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/sda/corrective-action-section/defective-part-description/defective-part-description.component.spec.ts
@@ -0,0 +1,39 @@
+import { DefectivePartDescriptionComponent } from './defective-part-description.component';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { AppStateService } from '../../../../common/services';
+import { MockAppStateService } from '../../../../common/services/mocks/mock-app-state.service';
+import { FormControlsModule } from '../../../../common/components/form-controls.module';
+import { ReactiveFormsModule, FormGroup } from '@angular/forms';
+import { SdaFormModule } from 'app/components/sda/sda-form.module';
+import * as models from '../../../../common/models';
+
+
+describe('SearchByCorrectiveActionComponent', () => {
+  let component: DefectivePartDescriptionComponent;
+  let fixture: ComponentFixture<DefectivePartDescriptionComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [],
+      imports: [ReactiveFormsModule, FormControlsModule, SdaFormModule ],
+      providers: [{ provide: AppStateService, useClass: MockAppStateService }]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DefectivePartDescriptionComponent);
+    component = fixture.componentInstance;
+
+    //Inputs
+    component.parent = new FormGroup({});
+    component.sda = {};
+    component.newSdaStus = models.Status.Open;
+
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
