c8d3744bb4eedbb8673e32b1ab54fae3da7011c9
Melinda <Melinda.Malmgren@aa.com>
2017-09-19 11:37:41 -0700

    Initial UI work

---
+++ src/app/components/alerts-save-searches/alerts-save-searches.component.ts
@@ -0,0 +1,76 @@
+import { Component, OnInit, ViewChild, ViewChildren } from '@angular/core';
+import { AccordionPanelComponent } from 'ngx-bootstrap';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AppStateService } from '../../common/services';
+import { ConfirmComponent } from '../../common/components/confirm/confirm.component';
+import { DialogService } from 'ng2-bootstrap-modal';
+import { Subject, Observable } from 'rxjs/Rx';
+import * as _ from 'lodash';
+
+@Component({
+  selector: 'aa-alerts-save-searches',
+  templateUrl: './alerts-save-searches.component.html',
+  styleUrls: ['./alerts-save-searches.component.less']
+})
+export class AlertsSaveSearchesComponent implements OnInit {
+  @ViewChildren(AccordionPanelComponent) panels: AccordionPanelComponent[];
+
+  searchByDateRange$: Subject<any> = new Subject();
+  searchBySDA$: Subject<any> = new Subject();
+  searchByAircraft$: Subject<any> = new Subject();
+  searchByPart$: Subject<any> = new Subject();
+  searchByCorrosion$: Subject<any> = new Subject();
+  searchByCorrectiveAction$: Subject<any> = new Subject();
+  criteria;
+
+  constructor(private fb: FormBuilder, private appStateService: AppStateService, private dialogService: DialogService) { }
+
+  ngOnInit() {
+    Observable.combineLatest(this.searchByDateRange$.startWith(undefined),
+      this.searchBySDA$.startWith(undefined),
+      this.searchByAircraft$.startWith(undefined),
+      this.searchByPart$.startWith(undefined),
+      this.searchByCorrosion$.startWith(undefined),
+      this.searchByCorrectiveAction$.startWith(undefined),
+      this.combineCriteria)
+      .subscribe(s => this.criteria = s);
+  }
+
+  combineCriteria(searchByDateRange, searchBySDA, searchByAircraft, searchByPart, searchByCorrosion, searchByCorrectiveAction) {
+      return { searchByDateRange, searchBySDA, searchByAircraft, searchByPart, searchByCorrosion, searchByCorrectiveAction}
+  }
+
+  expandCollapseAll(expandAll: boolean) {
+    this.panels.forEach(p => p.isOpen = expandAll);
+
+    return false;
+  }
+
+  searchAlerts() {
+    let hasCriteria = false;
+    const definedSections = _.pickBy(this.criteria, _.identity);  //Get all defined properties (searchByDateRange, etc)
+    _.forIn(definedSections, (value, key) => {  //Iterate over all sub-properties of that section (dateFrom, dateThrough, etc)
+      //Make sure they're A) defined and B) not an empty array
+      const validValues = _.pickBy(_.pickBy(value, _.identity), (x) => {
+        if (Array.isArray(x)) {
+          return x.length > 0;
+        }
+
+        return true;
+      });
+
+      if (!_.isEmpty(validValues)) {
+        hasCriteria = true;
+      }
+    });
+
+    if (!hasCriteria) {
+      this.dialogService.addDialog(ConfirmComponent, {
+        title: 'Search Filters',
+        message: 'Please input at least one search filter.'
+      })
+    } else {
+      this.appStateService.saveSdaSearchCriteria(this.criteria);
+    }
+  }
+}
--- src/app/components/search/search-options/search-options.component.ts
+++ src/app/components/search/search-options/search-options.component.ts
@@ -1,27 +1,15 @@
 import { Component, OnInit, ViewContainerRef } from '@angular/core';
-import createNumberMask from 'text-mask-addons/dist/createNumberMask';
 import { FormGroup, FormBuilder } from '@angular/forms';
-import { ToastrService } from 'ngx-toastr';
+
 @Component({
   selector: 'aa-search-options',
   templateUrl: './search-options.component.html',
   styleUrls: ['./search-options.component.less']
 })
-export class SearchOptionsComponent  implements OnInit {
-    createNumberMask = createNumberMask;
-    public numberMask = createNumberMask({
-        prefix: '',
-        allowDecimal: false,
-        includeThousandsSeparator: false,
-        allowLeadingZeroes: false
-    });
+export class SearchOptionsComponent implements OnInit {
 
-    constructor(private toastr: ToastrService) { }
+  constructor() { }
 
   ngOnInit() {
-  }
-
-  save() {
-        this.toastr.success('Form Saved', 'Success');
   }
 }
