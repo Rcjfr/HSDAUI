a6e04c98bb5649898bdb20152d8550504b452590
Malmgren, Melinda <Melinda.Malmgren@aa.com>
2017-11-02 10:32:39 -0700

    Merged PR 9171: Changing list of search report options

--- src/app/components/search/search-report/options.ts
+++ src/app/components/search/search-report/options.ts
@@ -1,101 +1,71 @@
 export const ReportOptions: any[] = [
-  //Create Date Range
-  {  key: 'dateFrom', display: 'SDA Create Date From'},
-  {  key: 'dateThrough', display: 'SDA Create Date Through'},
-
-  //SDA Form
-  {  key: 'id', display: 'SDA ID'},
-  {  key: 'station', display: 'Station'},
+  //General Information
+  {  key: 'sdrNumber', display: 'SDA Number'},
+  {  key: 'lineMaintenance', display: 'Line Maintenance'},
+  {  key: 'createDate', display: 'Create Date'},
   {  key: 'alertCode', display: 'Alert Code'},
-  {  key: 'sdrNumber', display: 'SDR #'},
-  {  key: 'department', display: 'Department'},
+  {  key: 'sdaId', display: 'SDA ID'},
   {  key: 'ataCode1', display: 'ATA Code 1'},
+  {  key: 'station', display: 'Station'},
   {  key: 'ataCode2', display: 'ATA Code 2'},
-  {  key: 'originator', display: 'Originator'},
-  {  key: 'fleet', display: 'Fleet'},
+  {  key: 'department', display: 'Department'},
+  {  key: 'defectDiscoveredDuring', display: 'Defect Discovered During'},
+  {  key: 'unscheduledMaintenanceDescription', display: 'Unscheduled Maintenance Description'},
+  {  key: 'nonRoutineNo', display: 'Non-Routine #'},
+  {  key: 'micNo', display: 'MIC #'},
   {  key: 'checkType', display: 'Check Type'},
-
-  //Aircraft
-  {  key: 'aircraftNo', display: 'Aircraft #'},
+  {  key: 'routineNo', display: 'Routine #'},
   {  key: 'manufacturer', display: 'Aircraft Manufacturer'},
   {  key: 'model', display: 'Aircraft Model/Series'},
   {  key: 'serialNo', display: 'Serial #'},
+  {  key: 'totalShipTime', display: 'Total Ship Time'},
+  {  key: 'cyles', display: 'Cycles'},
+  {  key: 'fleet', display: 'Fleet'},
+  {  key: 'originator', display: 'Originator'},
 
-  //Maintenance Type
-  {  key: 'defectDiscoveredDuringUnscheduled', display: 'Unscheduled Maintenance'},
-  {  key: 'defectDiscoveredDuringScheduled', display: 'Schedule Maintenance'},
-  {  key: 'lineMaintenance', display: 'Line Maintenance'},
-  {  key: 'unscheduledMaintenanceDescription', display: 'Unscheduled Maintenance Description'},
-  {  key: 'routineNo', display: 'Routine #'},
-  {  key: 'micNo', display: 'MIC #'},
-  {  key: 'nonRoutineNo', display: 'Non-Routine #'},
-
-  //Corrosion
-  {  key: 'isWideSpreadCorrosion', display: 'Widespread Corrosion'},
-  {  key: 'isPreviouslyBlended', display: 'Corrosion Previously Blended'},
-  {  key: 'corrosionTaskNo', display: 'Corrosion Task #'},
-  {  key: 'corrosionLevel', display: 'Corrosion Level'},
-  {  key: 'corrosionType', display: 'Corrosion Type'},
-  {  key: 'causesOfDamage', display: 'Cause of Damage'},
-  {  key: 'corrosionTypeOtherText', display: 'Cause of Damage Description'},
-
-  //CPCP Disposition
-  {  key: 'isNonCPCPRelatedEvent', display: 'Non-CPCP'},
-  {  key: 'isReviewComplete', display: 'Review Complete'},
-  {  key: 'isWideSpreadCorrosion', display: 'Widespread Corrosion'},
-  {  key: 'isCorrosionTaskNoCorrect', display: 'Is CPCP Task # correct'},
-  {  key: 'isCorrosionLevelCorrect', display: 'Is Corrosion Level correct'},
-  {  key: 'correctedCorrosionLevel', display: 'Corrected Corrosion Level'},
-  {  key: 'correctedCorrosionTaskNo', display: 'Correct Task #'},
-  {  key: 'corrosionLevelChangeReason', display: 'Reason For Change?'},
-  {  key: 'engineeringComments', display: 'Engineering Comments'},
-  {  key: 'qCFeedback', display: 'QC Feedback'},
-  {  key: 'submittedToQC', display: 'QC Notified'},
-  {  key: 'reviewer', display: 'Review Completed By'},
-
-  //Defect/Damage
+  //Description and Location of Defects or Damage
+  {  key: 'damageType', display: 'Damage Type'},
+  {  key: 'damageDescription', display: 'Damage Description'},
+  {  key: 'length', display: 'Defect Length'},
+  {  key: 'width', display: 'Defect Width'},
+  {  key: 'depth', display: 'Defect Depth'},
   {  key: 'aircraftStation', display: 'Aircraft Station'},
   {  key: 'stringer', display: 'Stringer'},
-  {  key: 'waterLine', display: 'WL'},
-  {  key: 'buttLine', display: 'BL'},
-  {  key: 'damageType', display: 'Damage Type'},
+  {  key: 'waterLine', display: 'Water Line'},
+  {  key: 'buttLine', display: 'Butt Line'},
+  {  key: 'manufacturerPartNo', display: 'MFG Part #'},
+  {  key: 'partDefective', display: 'Part Defective'},
+  {  key: 'manufacturerSerialNo', display: 'MFG Serial #'},
+  {  key: 'partTT', display: 'Part TT'},
+  {  key: 'partTSO', display: 'Part TSO'},
   {  key: 'detectionMethod', display: 'How Detected'},
-  {  key: 'lengthFrom', display: 'Defect Length From (in inches)'},
-  {  key: 'lengthTo', display: 'Defect Length To (in inches)'},
-  {  key: 'widthFrom', display: 'Defect Width From (in inches)'},
-  {  key: 'widthTo', display: 'Defect Width To (in inches)'},
-  {  key: 'depthFrom', display: 'Defect Depth From (in inches)'},
-  {  key: 'depthTo', display: 'Defect Depth To (in inches)'},
+
+  //Corrosion Prevention Control Program
+  {  key: 'isCPCPRelatedEvent', display: 'Is CPCP related event?'},
+  {  key: 'isWideSpreadCorrosion', display: 'Widespread Corrosion'},
+  {  key: 'corrosionLevel', display: 'Corrosion Level'},
+  {  key: 'isPreviouslyBlended', display: 'Corroded Area Previously Blended'},
+  {  key: 'corrosionTaskNo', display: 'Corrosion Task #'},
+  {  key: 'corrosionType', display: 'Type of Corrosion'},
+  {  key: 'causeOfDamage', display: 'Cause of Damage'},
+  {  key: 'damageDescription', display: 'Damage Description'},
+  {  key: 'floorBoardCondition', display: 'Floorboard condition after mat is removed'},
 
   //Corrective Action
   {  key: 'isDeferred', display: 'Deferred'},
-  {  key: 'deferralCode', display: 'Deferral Code'},
-  {  key: 'repairTypeDefective', display: 'Defective Part Replaced with Identical Part #'},
-  {  key: 'defectivePartDescription', display: 'Identical Part #'},
-  {  key: 'repairTypeModified', display: 'Modified Part # Installed'},
-  {  key: 'modifiedPartDescription', display: 'Modified Part #'},
-  {  key: 'repairTypeRepaired', display: 'Repaired'},
-  {  key: 'repairDescriptionType', display: 'Repaired Desc'},
-  {  key: 'isMajorRepair', display: 'Major Repair'},
-  {  key: 'majorRepairDescription', display: 'Major Repair Desc'},
-  {  key: 'repairDocumentType', display: 'Repair Document'},
-  {  key: 'isExternallyVisible', display: 'Externally Visible'},
-  {  key: 'repairHeightFrom', display: 'Repair Dimensions Height From (in inches)'},
-  {  key: 'repairHeightTo', display: 'Repair Dimensions Height To (in inches)'},
-  {  key: 'repairWidthFrom', display: 'Repair Dimensions Width From (in inches)'},
-  {  key: 'repairWidthTo', display: 'Repair Dimensions Width To (in inches)'},
-  {  key: 'chapFigRepairText', display: 'Chap/Fig/Repair'},
+  {  key: 'deferralCode', display: 'SCEPTRE Deferral Code'},
   {  key: 'deferralNo', display: 'Deferral #'},
+  {  key: 'repairType', display: 'Corrective Action Repair Type'},
+  {  key: 'defectivePartDescription', display: 'Defective Part Description'},
+  {  key: 'modifiedPartDescription', display: 'Modified Part Description'},
+  {  key: 'repairDescriptionType', display: 'Repaired Description Type'},
+  {  key: 'repairDocumentType', display: 'Repair Document'},
+  {  key: 'engineeringAuthorization', display: 'Engineering Authorization (EA)'},
+  {  key: 'isExternallyVisible', display: 'Externally Visible?'},
+  {  key: 'repairHeight', display: 'Repair Height (inches)'},
+  {  key: 'repairWidth', display: 'Repair Width (inches)'},
+  {  key: 'isMajorRepair', display: 'Major Repair'},
+  {  key: 'majorRepairDescription', display: 'Major Repair Description'}
 
-  //Status
-  {  key: 'status', display: 'Status'},
-
-  //Part
-  {  key: 'manufacturerPartNo', display: 'MFG Part #'},
-  {  key: 'manufacturerSerialNo', display: 'MFG Serial #'},
-  {  key: 'partDefective', display: 'Part Defective'},
-
-  //Part
-  {  key: 'useAndOperator', display: 'And/Or Option'},
 
 ];
