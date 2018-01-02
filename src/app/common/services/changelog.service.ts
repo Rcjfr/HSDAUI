f0835ff57161bc7e6d4e2e8891d2dc9c68c6d0d0
Agarwal, Anoop <Anoop.Agarwal@aa.com>
2018-01-02 13:01:53 -0700

    Merged PR 10373: Merge Anoop/Release5/ITR3/US556200 to master
    please approve

--- src/app/common/services/changelog.service.ts
+++ src/app/common/services/changelog.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { environment } from '@env/environment';
 import { IChangeLog } from '@app/common/models/change-log.model';
+import { Helper } from '@app/common/helper';
 
 @Injectable()
 export class ChangeLog {
@@ -10,8 +11,8 @@ export class ChangeLog {
   constructor(private http: HttpClient) { }
 
   getChangeLog(id: number, version: number): Observable<IChangeLog[]> {
-
-    return this.http.get<IChangeLog[]>(`${this.endPointUrl}/${id}/version/${version}`);
+    return this.http.get(`${this.endPointUrl}/${id}/version/${version}`, { responseType: 'text' })
+    .map((result) => Helper.Deserialize(result));
   };
 
 }
