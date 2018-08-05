c8e05a25d7dd4018d185f6b1787c2e0f5d4bba8b
Chebrolu, Azeet <Azeet.Chebrolu@aa.com>
2018-08-04 22:21:45 -0700

    Merged PR 17527: Merge azeet/Release6/Iteration4/DataMigration to master

--- src/app/components/reports/twd-report/twd-report-search/twd-report-search.component.ts
+++ src/app/components/reports/twd-report/twd-report-search/twd-report-search.component.ts
@@ -16,8 +16,8 @@ import { IAircraftInfo, Status } from '@app/common/models';
 import * as models from '@app/common/models';
 import { Observable, Observer } from 'rxjs/Rx';
 import { Expressions } from '@app/common/validators/generic-validator';
-import { ISearchCriteria} from '@app/common/models/search/search-criteria.model';
-import {ILazyLoadEvent} from '@app/common/models/lazy-load-event.model';
+import { ISearchCriteria } from '@app/common/models/search/search-criteria.model';
+import { ILazyLoadEvent } from '@app/common/models/lazy-load-event.model';
 import { ConfirmComponent } from '@app/common/components/confirm/confirm.component';
 import { DialogService } from 'ng2-bootstrap-modal';
 import * as _ from 'lodash';
@@ -35,95 +35,95 @@ export class TwdReportSearchComponent implements OnInit {
   defaultSortOrder = 1;
 
   twdReportSearchForm: FormGroup;
-formSubmitted: boolean;
+  formSubmitted: boolean;
 
-@Output() onShowTwd = new EventEmitter<ISearchCriteria>();
-@Output() onShowTwdPdf = new EventEmitter<ISearchCriteria>();
-@Output() onShowTwdExcel = new EventEmitter<ISearchCriteria>();
-loading$: Observable<boolean>;
-noseNumbers$: Observable<models.IAircraftInfo[]>;
-constructor( private dialogService: DialogService, public appStateService: AppStateService) {
-  this.twdReportSearchForm = new FormGroup({
-    fleet: new FormControl(''),
-    aircraftNo: new FormControl('', [Validators.maxLength(5), Validators.pattern(Expressions.Alphanumerics)]),
-  });
-
-}
-
-ngOnInit() {
-  this.loading$ = this.appStateService.getSelectedAlertLoading();
-  this.noseNumbers$ = Observable.create((observer: Observer<string>) => {
-    observer.next(this.twdReportSearchForm.get('aircraftNo').value);
-  })
-    .switchMap(token => {
-      this.appStateService.loadNoseNumbers(token);
-
-      return this.appStateService.getNoseNumbers();
+  @Output() onShowTwd = new EventEmitter<ISearchCriteria>();
+  @Output() onShowTwdPdf = new EventEmitter<ISearchCriteria>();
+  @Output() onShowTwdExcel = new EventEmitter<ISearchCriteria>();
+  loading$: Observable<boolean>;
+  noseNumbers$: Observable<models.IAircraftInfo[]>;
+  constructor(private dialogService: DialogService, public appStateService: AppStateService) {
+    this.twdReportSearchForm = new FormGroup({
+      fleet: new FormControl(''),
+      aircraftNo: new FormControl('', [Validators.maxLength(5), Validators.pattern(Expressions.Alphanumerics)]),
     });
+
+  }
+
+  ngOnInit() {
+    this.loading$ = this.appStateService.getSelectedAlertLoading();
+    this.noseNumbers$ = Observable.create((observer: Observer<string>) => {
+      observer.next(this.twdReportSearchForm.get('aircraftNo').value);
+    })
+      .switchMap(token => {
+        this.appStateService.loadNoseNumbers(token);
+
+        return this.appStateService.getNoseNumbers();
+      });
 
     this.fleet$ = this.appStateService.getFleet();
     this.twdReportSearchForm.valueChanges.subscribe(form => {
-    form.fleet = _.compact(form.fleet)
-  })
+      form.fleet = _.compact(form.fleet)
+    })
 
-}
-
-getDefaultPageData(): ILazyLoadEvent {
-  return {
-    first: 0,
-    rows: this.defaultPageSize,
-    sortField: this.defaultSortColumn,
-    sortOrder: this.defaultSortOrder
   }
-}
 
-
-showTwdReport(isExcel = false, isPdf = false) {
-
-  this.formSubmitted = true;
-  this.markAsDirty(this.twdReportSearchForm);
-  if (this.twdReportSearchForm.valid) {
-    if (this.twdReportSearchForm.controls.aircraftNo.value) {
-      this.searchCriteria.searchByAircraft = { aircraftNo: this.twdReportSearchForm.controls.aircraftNo.value };
-    }
-    if (this.twdReportSearchForm.controls.fleet.value) {
-      this.searchCriteria.searchBySda = { fleet: `${this.twdReportSearchForm.controls.fleet.value}*` };
-    }
-    this.searchCriteria.searchByDTE = {dteStatus: [1]};
-    //this.searchCriteria.searchByCorrectiveAction = { isMajorRepair: 1}; //THIS IS THE DEFAULT FOR ANY SEARCHBYDTE SECTION
-    this.searchCriteria.pageData = this.getDefaultPageData();
-    if (isExcel) {
-      this.onShowTwdExcel.emit(this.searchCriteria);
-    } else if (isPdf) {
-      this.onShowTwdPdf.emit(this.searchCriteria);
-    } else {
-      this.onShowTwd.emit(this.searchCriteria);
+  getDefaultPageData(): ILazyLoadEvent {
+    return {
+      first: 0,
+      rows: this.defaultPageSize,
+      sortField: this.defaultSortColumn,
+      sortOrder: this.defaultSortOrder
     }
   }
-}
 
 
-markAsDirty(group: FormGroup | FormArray) {
-  group.markAsDirty();
-  for (const i in group.controls) {
-    if (group.controls[i] instanceof FormControl) {
-      group.controls[i].markAsDirty();
-    } else {
-      this.markAsDirty(group.controls[i]);
+  showTwdReport(isExcel = false, isPdf = false) {
+
+    this.formSubmitted = true;
+    this.markAsDirty(this.twdReportSearchForm);
+    if (this.twdReportSearchForm.valid) {
+      this.searchCriteria = { searchByDTE: { dteStatus: [1] } };
+      if (this.twdReportSearchForm.controls.aircraftNo.value) {
+        this.searchCriteria.searchByAircraft = { aircraftNo: this.twdReportSearchForm.controls.aircraftNo.value };
+      }
+      if (this.twdReportSearchForm.controls.fleet.value) {
+        this.searchCriteria.searchBySda = { fleet: `${this.twdReportSearchForm.controls.fleet.value}*` };
+      }
+      //this.searchCriteria.searchByCorrectiveAction = { isMajorRepair: 1}; //THIS IS THE DEFAULT FOR ANY SEARCHBYDTE SECTION
+      this.searchCriteria.pageData = this.getDefaultPageData();
+      if (isExcel) {
+        this.onShowTwdExcel.emit(this.searchCriteria);
+      } else if (isPdf) {
+        this.onShowTwdPdf.emit(this.searchCriteria);
+      } else {
+        this.onShowTwd.emit(this.searchCriteria);
+      }
     }
   }
-}
 
-noseValidator(): string {
-  if (this.twdReportSearchForm.get('aircraftNo').errors && this.formSubmitted || (this.twdReportSearchForm.get('aircraftNo').errors &&
-   (this.twdReportSearchForm.get('aircraftNo').dirty || this.twdReportSearchForm.get('aircraftNo').touched))) {
+
+  markAsDirty(group: FormGroup | FormArray) {
+    group.markAsDirty();
+    for (const i in group.controls) {
+      if (group.controls[i] instanceof FormControl) {
+        group.controls[i].markAsDirty();
+      } else {
+        this.markAsDirty(group.controls[i]);
+      }
+    }
+  }
+
+  noseValidator(): string {
+    if (this.twdReportSearchForm.get('aircraftNo').errors && this.formSubmitted || (this.twdReportSearchForm.get('aircraftNo').errors &&
+      (this.twdReportSearchForm.get('aircraftNo').dirty || this.twdReportSearchForm.get('aircraftNo').touched))) {
       if (this.twdReportSearchForm.get('aircraftNo').errors.maxlength) {
         return 'Aircraft Nose # must not be more than 5 characters.';
       }
       if (this.twdReportSearchForm.get('aircraftNo').errors.pattern) {
         return 'Aircraft Nose # must contain only alphanumerics.';
       }
+    }
   }
-}
 
 }
