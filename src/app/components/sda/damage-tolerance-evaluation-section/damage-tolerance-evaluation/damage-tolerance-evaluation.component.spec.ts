1736a2e29c213ac3db67ec1e764c4f6f326930eb
Robert Frangella <robert.frangella@aa.com>
2022-03-01 10:45:34 -0700

    Fix Build Errors (additional Linting)

--- src/app/components/sda/damage-tolerance-evaluation-section/damage-tolerance-evaluation/damage-tolerance-evaluation.component.spec.ts
+++ src/app/components/sda/damage-tolerance-evaluation-section/damage-tolerance-evaluation/damage-tolerance-evaluation.component.spec.ts
@@ -98,4 +98,4 @@ describe('DamageToleranceEvaluationComponent', () => {
         DteThresholdItemComponent,
         DteThresholdItemsArrayComponent,
         DteMonitorItemComponent,
-        DteThresholdItemsArrayComponent,       
+        DteThresholdItemsArrayComponent
